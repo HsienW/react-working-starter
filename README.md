7e54c9fbf692ba61b429c7119389e7cded04f03a
Wallace H Wu <wallace_h_wu@wistron.com>
2020-04-23 07:04:51 +0800

    create master

---
+++ README.md
@@ -0,0 +1,1 @@
+#React Working Starter
---
+++ src/containers/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Main = () => {
+    return (
+        <div>
+            <h2>React-Working-Starter is working!</h2>
+        </div>
+    );
+};
